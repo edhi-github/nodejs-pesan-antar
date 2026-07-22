9eb539ad38e58b3d6b8392ebd7e61c8f0a21804e
edhi <edhi.heriyaman@gmai.com>
2026-07-22 13:52:04 +0700

    tambah endpoint untuk daftar dari landing page

--- config_landingpage.js
+++ config_landingpage.js
@@ -1,8 +1,3 @@
 // config_landingpage.js
 
-// URL Backend Server Express Anda
-// Jika dijalankan di lokal, gunakan http://localhost:3000
-// Jika di server live (Railway/VPS), sesuaikan domain backend Anda
-const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-    ? 'http://localhost:3000'
-    : 'https://nodejs-pesan-antar.up.railway.app'; // Ganti dengan domain Railway backend Anda
+const API_URL = 'https://nodejs-pesan-antar.up.railway.app';
